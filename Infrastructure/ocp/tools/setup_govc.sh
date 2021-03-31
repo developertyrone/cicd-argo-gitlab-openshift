fa5f3ad3f10329c19b79f1dd1c58a188db8a8f55
Tyrone Chan <admin@tyronechan.com>
2021-04-01 00:10:23 +0800

    fix

--- Infrastructure/ocp/tools/setup_govc.sh
+++ Infrastructure/ocp/tools/setup_govc.sh
@@ -4,8 +4,7 @@ wget https://github.com/vmware/govmomi/releases/download/v0.24.0/govc_linux_amd6
 gunzip -d govc_linux_amd64.gz
 chmod +x govc_linux_amd64
 mv govc_linux_amd64 /usr/local/sbin/govc
-
-export GOVC_URL=vcenter.local
-export GOVC_USERNAME=administrator@vcenter.local 
-export GOVC_PASSWORD=asdfasfafas
-export GOVC_INSECURE=true
+export GOVC_URL="vcenter.local"
+export GOVC_USERNAME="administrator@vcenter.local "
+export GOVC_PASSWORD="asdfasfafas"
+export GOVC_INSECURE="true"
